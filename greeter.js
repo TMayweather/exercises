0b0253692fd73eaf3944aa89a1d72d5446cce8aa
Tnea Mayweather <tnea.mayweather@hotmail.com>
2018-02-03 16:15:00 -0500

    add greeter.js

---
+++ greeter.js
@@ -0,0 +1,12 @@
+var name =  prompt('What is your name');
+
+var checkName = function() {
+if(name === "") {
+  name = prompt('What is your name, for real this time?');
+
+  checkName();
+}
+}
+
+checkName();
+  alert('Hello ' + name);
